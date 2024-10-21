f4f53230d901bb773bc1bb13e0beaf06175c2704
VictorIrmand <vini0003@stud.kea.dk>
2024-10-21 09:26:48 +0200

    martin

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello MaiMann");
         System.out.println("Hello irmand");
         System.out.println("Dav");
+        System.out.println("Martin");
     }
 }
