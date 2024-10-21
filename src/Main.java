9bf5ca1767c029fc9f3a4a44bb31cff8200219e1
MartinMD-Git <mada0008@stud.kea.dk>
2024-10-21 10:02:58 +0200

    Victoria

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("victoria");
         System.out.println("sjknsfdjkfjk");
         System.out.println("Martinelli");
+        System.out.println("jaajaja");
 
     }
 }
